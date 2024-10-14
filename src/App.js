ace8248c5fe94d623c06f5d4e32124f583875dfa
mielindev <phamvietquang.dev@gmail.com>
2024-10-14 20:10:07 +0700

    set background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import XucXacGame from "./XucXacGame/XucXacGame";
+import bgImage from "./imageXucXac/bgGame.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        height: "100vh",
+      }}
+    >
+      <XucXacGame />
     </div>
   );
 }
---
+++ src/XucXacGame/XucXacGame.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function XucXacGame() {
+  return <div>XucXacGame</div>;
+}
